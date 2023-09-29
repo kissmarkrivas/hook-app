379a8bd23ffd39de43deb7252262f022b0786f0b
kissmark <rivaskissmark@gmail.com>
2023-09-28 22:32:57 -0500

    add list todo

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,26 +1,25 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-export const useForm = (initialForm = {} ) => {
-  
-  const [formState, setFormState] = useState(initialForm);
+export const useForm = ( initialForm = {} ) => {
 
+  const [ formState, setFormState ] = useState( initialForm );
 
-  const onInputChange = ({target})=> {
-    const { name,value} = target;
-    setFormState({
-      ...formState,
-      [name]:value
-    })
+  const onInputChange = ({ target }) => {
+      const { name, value } = target;
+      setFormState({
+          ...formState,
+          [ name ]: value
+      });
   }
 
   const onResetForm = () => {
-    setFormState( initialForm  )
+      setFormState( initialForm );
   }
 
-
   return {
-    formState,
-    onInputChange,
-    onResetForm
+      ...formState,
+      formState,
+      onInputChange,
+      onResetForm,
   }
 }
